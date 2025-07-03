4477f8ec0dca25bae2f23f7c7e1be790f12995a3
swechchha <nigam316@gmail.com>
2025-07-03 08:23:31 +0530

    Add problem4

---
+++ GreedyApproach/Problem4_JobSequencingProblem.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Job {
+    String id;
+    int deadline, profit;
+
+    Job(String i, int d, int p) {
+        id = i;
+        deadline = d;
+        profit = p;
+    }
+}
+
+public class Problem4_JobSequencingProblem {
+    public static void main(String[] args) {
+        Job[] jobs = {
+            new Job("a", 4, 20),
+            new Job("b", 1, 10),
+            new Job("c", 1, 40),
+            new Job("d", 1, 30)
+        };
+        Arrays.sort(jobs, (a, b) -> b.profit - a.profit);
+        int maxDeadline = Arrays.stream(jobs).mapToInt(j -> j.deadline).max().getAsInt();
+        boolean[] slots = new boolean[maxDeadline + 1];
+        int totalProfit = 0;
+        List<String> scheduledJobs = new ArrayList<>();
+
+        for (Job job : jobs) {
+            
+            for (int j = job.deadline; j > 0; j--) {
+                if (!slots[j]) {
+                    slots[j] = true;
+                    totalProfit += job.profit;
+                    scheduledJobs.add(job.id);
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Scheduled Jobs: " + scheduledJobs);
+        System.out.println("Total Profit: " + totalProfit);
+    }
+}
